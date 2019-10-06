95227d7551a5ed9a70f06b7c3d9c83fa39f1effe
Oz Blumenfeld <ozb077@gmail.com>
2019-10-06 23:04:37 +0300

    Fixing issues still need to fix EmployeeControllerTest.java

--- demo/src/main/java/com/example/demo/Employee.java
+++ demo/src/main/java/com/example/demo/Employee.java
@@ -4,18 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotNull
+    @NonNull
     private String firstName;
-    @NotNull
+    @NonNull
     private String lastName;
     // Standard constructors and destructors
 
--- demo/src/main/java/com/example/demo/EmployeeControllerTests.java
+++ demo/src/main/java/com/example/demo/EmployeeControllerTests.java
@@ -1,23 +1,38 @@
-// package com.example.demo;
+package com.example.demo;
 
-// @RunWith(SpringRunner.class)
-// @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-// @AutoConfigureMockMvc
-// public class EmployeeControllerTests {
-//     @Autowired
-//     private MockMvc mvc;
-//     @Test
-//     @WithMockUser()
-//     public void givenNoEmployee_whenCreateEmployee_thenEmployeeCreated() throws Exception {
-//         mvc.perform(post("/employees").content(
-//             new ObjectMapper().writeValueAsString(new Employee("First", "Last"))
-//             .with(csrf()))
-//           .contentType(MediaType.APPLICATION_JSON)
-//           .accept(MediaType.APPLICATION_JSON))
-//           .andExpect(MockMvcResultMatchers.status()
-//             .isCreated())
-//           .andExpect(jsonPath("$.firstName", is("First")))
-//           .andExpect(jsonPath("$.lastName", is("Last")));
-//     }
-//     // other tests as necessary
-// }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.context.support.WithMockUser;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerTests {
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    @WithMockUser()
+    public void givenNoEmployee_whenCreateEmployee_thenEmployeeCreated() throws Exception {
+        mvc.perform(post("/employees").content(
+            new ObjectMapper().writeValueAsString(new Employee("First", "Last"))
+            .with(csrf()))
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+          .andExpect(MockMvcResultMatchers.status()
+            .isCreated())
+          .andExpect(jsonPath("$.firstName", is("First")))
+          .andExpect(jsonPath("$.lastName", is("Last")));
+    }
+    // other tests as necessary
+}
